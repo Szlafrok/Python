2dc1e3f29116b6dceca2e7aaf03becc3d40b28b1
PiotrekP <kamiennygigant@gmail.com>
2024-10-05 16:47:52 +0200

    treść zadania 2

---
+++ Lekcja2/tresc_zadania.py
@@ -0,0 +1,31 @@
+# Dane są typy danych w języku Python:
+
+int() # liczba całkowita
+float() # liczba dziesiętna / zmiennoprzecinkowa
+bool() # wartość logczina True/False
+str() # tekst
+
+# Każda spośród czterech powyższych funkcji konwertuje podaną wartość
+# na wskazany typ danych. Przykładowo:
+
+zmienna = int("123")
+print(zmienna) # wyrzuci liczbę całkowitą 123.
+
+# Proszę zastanowić się nad skutkami konwersji podanych w lewej kolumnie
+# typów danych na typy danych w prawej kolumnie. Kiedy otrzymamy błąd? Kiedy
+# konwersja jest możliwa? Może liczba się zaokrągli? Może dla różnych podanych
+# danych skutki będą różne?
+
+# Rozwiązania proszę opisać w komentarzach lub pisemnie (np. notatnik, word, lub sam
+# email) i przesłać na email:
+
+email = "kamiennygigant@gmail.com"
+
+
+# int -> str
+# float -> str
+# str -> bool
+# str -> int
+# str - > float
+# bool -> float
+# bool -> int
