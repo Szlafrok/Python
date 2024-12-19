6a5402bfbf970d5f5b1739c1c77c69eb97eefb23
PiotrekP <kamiennygigant@gmail.com>
2024-12-19 19:39:11 +0100

    w trakcie lekcji 13

--- Lekcja13/pygame_kontynuacja.py
+++ Lekcja13/pygame_kontynuacja.py
@@ -13,14 +13,26 @@ pygame.display.set_caption('Pierwsza gra') # Określenie nazwy okna, które się
 clock = pygame.time.Clock()
 
 def load_image(img_path, position): # img_path - ścieżka z obrazka
-    pass
+    image = pygame.image.load(img_path)
+    surface = image.convert()
 
-def print_image():
-    pass
+    transparent_color = (0, 0, 0)
+    surface.set_colorkey((transparent_color)) # Zamienia kolor czarny na przezroczysty
+
+    rect = surface.get_rect(center = position) # Pozycja wyświetlania obiektu
+
+    return [image, surface, rect]
+
+def print_image(img_list):
+    image, surface, rect = img_list
+    screen_surface.blit(surface, rect)
+    return
 
 #################################################################
 
-load_image("Lekcja12/grafiki/player.png", None)
+player_pos = [0, 0]#[SZEROKOSC // 2, WYSOKOSC // 2]
+
+player = load_image("Lekcja12/grafiki/player.png", player_pos)
 
 status_gry = True # Flaga określająca, czy gra jest kontynuowana
 
@@ -31,6 +43,8 @@ while status_gry:
     for event in events:
         if event.type == pygame.QUIT:
             status_gry = False
+
+    print_image(player)
 
     pygame.display.update() # Aktualizuje ekran
 
