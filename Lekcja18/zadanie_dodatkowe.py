adc3c3c5ae3262f52e39961b077cfb110990fe4b
PiotrekP <kamiennygigant@gmail.com>
2025-01-30 20:18:37 +0100

    zadanie dodatkowe

---
+++ Lekcja18/zadanie_dodatkowe.py
@@ -0,0 +1,14 @@
+# (+3 pkt)
+
+# Spójny ciąg rosnący to ciąg kolejnych elementów listy L, w którym każdy
+# kolejny element jest większy od poprzedniego.
+
+# Proszę napisać funkcję, która przyjmuje jako argument listę L zawierającą
+# liczby naturalne i zwraca długość najdłuższego spójnego ciągu rosnącego.
+
+# Dla listy:
+[2, 4, 3, 4, 5, 1]
+
+# Najdłuższym spójnym ciągiem rosnącym jest [3, 4, 5] - ma długość 3.
+
+# Zadanie można wysłać mi do północy ;)
