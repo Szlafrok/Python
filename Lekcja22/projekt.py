7aea0f3678e87b733e9ad8118e108178991e89ca
PiotrekP <polanski.piotrek04@gmail.com>
2025-02-27 19:24:07 +0100

    w trakcie lekcji 22

---
+++ Lekcja22/projekt.py
@@ -0,0 +1,50 @@
+"""Proszę napisać klasę Zwierze(), która:
+- w konstruktorze przyjmie argumenty: imie oraz wiek.
+- ma metodę dzwiek(), która wypisze do konsoli, że zwierzę o takim imieniu robi dźwięk.
+- ma metodę jedz(), która wypisze do konsoli, że zwierzę o takim imieniu je.
+
+"""
+
+class Zwierze():
+
+    def __init__(self, imie, wiek):
+        self.imie = imie
+        self.wiek = wiek
+
+    def dzwiek(self):
+        print(f"{self.imie} wydaje dzikie odgłosy (telewizor poszedł w drzazgi)")
+
+    def jedz(self):
+        print(f"{self.imie} ma {self.wiek} lat i wcina cokolwiek zostało z mojej kanapy (om nom nom)")
+
+
+class Pies(Zwierze):
+
+    def __init__(self, imie, wiek, rasa):
+        super().__init__(imie, wiek)
+        self.rasa = rasa
+
+    def jaka_rasa(self):
+        print(self.rasa)
+
+
+"""Proszę napisać klasę Papuga, która będzie dziedziczyła
+z klasy Zwierze i dodatkowo będzie mieć:
+- zmienną kolor, która będzie trzymać kolor piór
+- metodę papuguj(), która opowie jakiś żart."""
+
+
+
+
+piesek = Zwierze("Szaman", 4)
+
+print(piesek.imie)
+piesek.dzwiek()
+piesek.jedz()
+
+lepszy_piesek = Pies("Kwiat", 6, "Doberman")
+lepszy_piesek.jedz()
+
+
+piesek.jaka_rasa()
+lepszy_piesek.jaka_rasa()
