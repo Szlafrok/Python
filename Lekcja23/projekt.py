2af9d9634e4d36821fbef67bb3da824ef2715665
PiotrekP <kamiennygigant@gmail.com>
2025-03-06 19:53:58 +0100

    w trakcie lekcji

--- Lekcja23/projekt.py
+++ Lekcja23/projekt.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 class Postac:
     def __init__(self):
@@ -15,5 +15,69 @@ class Postac:
             print(f"{self.nazwa} atakuje {przeciwnik.nazwa} i zadaje {atak} obrażeń.")
             przeciwnik.hp -= atak
 
+        if self == przeciwnik:
+            print("Mordo czemu naparzasz samego siebie?")
+
 # Proszę napisać klasy: Gracz oraz Przeciwnik, które będą dziedziczyły po klasie Postać i wywołają w 
-# swoim konstruktorze jej konstruktor.
+# swoim konstruktorze jej konstruktor. 
+
+class Gracz(Postac):
+    def __init__(self, hp = 10, max_hp = 10):
+        super().__init__()
+        self.hp = hp
+        self.max_hp = max_hp
+        self.nazwa = input("Podaj imię gracza: ")
+
+    def przedstaw_sie(self):
+        print(f"Nazywam się {self.nazwa} i mam {self.hp}/{self.max_hp}")
+
+    def zjedz(self):
+        self.hp += 2 # self.hp = self.hp + 2
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+        print(f"Gracz {self.nazwa} wcina kebaba i ma teraz {self.hp}/{self.max_hp} HP.")
+
+
+    def walka(self, przeciwnik):
+        czy_walka = True
+        while czy_walka:
+            print(f"Życie gracza: {self.hp}")
+            print(f"Życie przeciwnika: {przeciwnik.hp}")
+            akcja = input("Wybierz: atak/ucieczka")
+            if akcja == "atak":
+                self.atakuj(przeciwnik)
+                if przeciwnik.hp < 1:
+                    print(f"{przeciwnik.nazwa} poszedł spać (a mimir)")
+                    return True
+                przeciwnik.atakuj(self)
+            elif akcja == "ucieczka":
+                print(f"{self.nazwa} ucieka fiatem 126p (pyrpyrpyrpyr)")
+                czy_walka = False
+            else:
+                print("W sumie to nie wiesz co robić i pomijasz turę :V")
+                przeciwnik.atakuj(self)
+            
+            if self.hp < 1:
+                print(f"{self.nazwa} poszedł spać (a mimir)")
+                return False
+
+
+class Przeciwnik(Postac):
+    def __init__(self, hp = 6, max_hp = 6):
+        super().__init__()
+        self.nazwa = choice(["Goblin", "Małpa", "Lizard", "Ork", "Amir", "Steve", "Chochlik", "Menel", "Diablik", "Sans"])
+        self.max_hp = max_hp
+        self.hp = hp
+
+
+# ziomeczek = Gracz(15, 15)
+# ziomeczek.przedstaw_sie()
+# ziomeczek.atakuj(ziomeczek)
+# ziomeczek.przedstaw_sie()
+# ziomeczek.zjedz()
+
+ziomeczek = Gracz()
+gra = True
+
+while gra:
+    przeciwnik = Przeciwnik()
