43882473212f64581643f3cf87cb9e9049cfa004
PiotrekP <kamiennygigant@gmail.com>
2025-05-15 20:19:37 +0200

    zadanie domowe

--- Lekcja32/kino.py
+++ Lekcja32/kino.py
@@ -5,8 +5,41 @@ self.name i self.order odpowiednio imię i zamówienie klienta.
 Proszę utworzyć klasę CinemaQueue, która:
 - w konstruktorze utworzy kolejkę do zmiennej self.queue
 - w metodzie is_empty zwróci informację True/False, zależnie czy kolejka jest pusta
-- w metodzie add_customer doda klienta do kolejki. Metoda powinna przyjmować klienta jako argument.
-- w metodzie remove_customer, o ile kolejka nie jest pusta, zwróci pierwszego klienta w kolejce i usunie go z tej kolejki
-- w metodzie next_customer_order, o ile kolejka nie jest pusta, podejrzy zamówienie pierwszego klienta i zwróci je
+- w metodzie add_customer doda klienta do kolejki. Metoda powinna przyjmować klienta 
+jako argument.
+- w metodzie remove_customer, o ile kolejka nie jest pusta, zwróci pierwszego klienta 
+w kolejce i usunie go z tej kolejki
+- w metodzie next_customer_order, o ile kolejka nie jest pusta, podejrzy zamówienie 
+pierwszego klienta i zwróci je
 
 """
+from pokazowe import *
+
+class Customer():
+    def __init__(self, name, order):
+        self.name = name
+        self.order = order
+
+class CinemaQueue():
+    def __init__(self):
+        self.queue = Queue()
+
+    def is_empty(self):
+        return self.queue.is_empty()
+    
+    def add_customer(self, customer):
+        self.queue.enqueue(customer)
+
+    def remove_customer(self):
+        if not self.is_empty():
+            customer = self.queue.dequeue()
+            print(customer.name, customer.order)
+            return customer
+        
+    def next_customer_order(self):
+        if not self.is_empty():
+            next_customer = self.queue.peek()
+            return next_customer.order
+        
+        
+    
--- Lekcja32/zadania.md
+++ Lekcja32/zadania.md
@@ -1,9 +1,13 @@
 ### Zadanie domowe
-*Termin wykonania: 29.05 / 4 pkt*
 
-Korzystając z implementacji stosu z lekcji, proszę napisać funkcję **palindrom(s: str) -> bool**, która przyjmuje jako argument stringa s. Funkcja powinna określać, czy podany string jest palindromem (czyli od tyłu czyta się je tak samo jak od przodu).
+*Termin wykonania: 29.05 / 4 pkt*
+1. Korzystając z implementacji stosu z lekcji, proszę napisać funkcję **palindrom(s: str) -> bool**, która przyjmuje jako argument stringa s. Funkcja powinna określać, czy podany string jest palindromem (czyli od tyłu czyta się je tak samo jak od przodu).
+
+*Termin wykonania: 29.05 / 2 pkt*
+2. Proszę napisać polecenia sprawdzające poprawne działanie CinemaQueue w pliku kino.py
+
 
 ### Zadanie dodatkowe
 *Termin wykonania: 29.05 / +3 pkt*
 
-Pojawi się po zajęciach!
+Pojawi się 16.05 w godzinach popołudniowych.
