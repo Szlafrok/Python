8207879dc394766b899c6881c0f08eb3e624bcb1
PiotrekP <kamiennygigant@gmail.com>
2025-06-05 19:17:17 +0200

    przerwa lekcja 35

--- Lekcja35/projekt.py
+++ Lekcja35/projekt.py
@@ -1,18 +1,54 @@
+import random
+
+lista = []
+
+for i in range(10):
+    val = random.randint(1, 50000)
+    lista.append(val)
+
+print(lista)
+
+for i in range(10):
+    if i%2 == 0:
+        print(lista[i])
+    
+
+
 # Tworzenie listy
+papaja = [1, 5, 2137, "maluch", None, "kremówka"]
+print(papaja)
 
 # append
+papaja.append("kebab")
+print(papaja)
 
 # extend
+odjazd = ["mcqueen", "cinquecento", "da_ktos_blika", "na_kebaba"]
+odjazd.extend(papaja)
+print(odjazd)
 
 # insert
-
-# remove
+print(papaja)
+papaja.insert(1, "kaczucha")
+print(papaja)
 
 # pop
+papaja.pop(4)
+print(papaja)
+
+# remove
+papaja.extend(["kebab", "kebab"])
+papaja.remove("kebab")
+print(papaja)
 
 # index
+print(papaja.index(None))
+
+#papaja.pop(papaja.index("kremówka"))
 
 # count
+
+
 
 # sort
 
