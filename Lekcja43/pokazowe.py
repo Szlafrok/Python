303505e8d0db813377880a8aa5700ec5ec16a138
PiotrekP <kamiennygigant@gmail.com>
2025-08-07 19:06:36 +0200

    przerwa

--- Lekcja43/pokazowe.py
+++ Lekcja43/pokazowe.py
@@ -35,12 +35,18 @@ print(lat_2, lon_2, name_2, country_2)
 
 def get_weather_info(lat, lon, API_KEY):
     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&lang=PL&units=metric")
-    print(response.status_code)
-    pprint(response.json())
+    # print(response.status_code)
+    # pprint(response.json())
     response_json = response.json()
     weather = response_json['weather'][0]['description']   # pogoda
     temperature = response_json['main']['feels_like']     # temperatura odczuwalna
-    pressure = # ciśnienie
-    humidity = # wilgotność
+    pressure = response_json['main']['pressure'] # ciśnienie
+    humidity = response_json['main']['humidity'] # wilgotność
+    return weather, temperature, pressure, humidity
 
-get_weather_info(lat_2, lon_2, API_KEY)
+weather, temperature, preesure, humidity = get_weather_info(lat_2, lon_2, API_KEY)
+
+print(f"Pogoda: {weather}")
+print(f"Temperatura: {temperature} st. C")
+print(f"Ciśnienie: {preesure} hPa")
+print(f"Wilgotność: {humidity}%")
