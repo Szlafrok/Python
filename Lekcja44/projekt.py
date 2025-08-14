ab092c692abea8b3d3bfdf8ea72d7ea6f032ce89
PiotrekP <kamiennygigant@gmail.com>
2025-08-14 19:56:10 +0200

    aktualizacja w czasie lekcji

--- Lekcja44/projekt.py
+++ Lekcja44/projekt.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from fpdf import FPDF
 
 def get_pokemon_info(pokemon_name):
-    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}/"
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
     response = requests.get(url)
     return response.json()
 
@@ -20,11 +20,17 @@ def scrap_pokemon_list():
         pokemon = (columns[2].text[:-1], f"No.{columns[0].text[:-1]}")
         pokemons_list.append(pokemon)
         #print(pokemon)
-    pokemons_without_mega = [pokemon for pokemon in pokemons_list if "Mega" not in pokemon[0]]
+    pokemons_without_mega = [pokemon for pokemon in pokemons_list if ("Mega" not in pokemon[0] and "Gigantamax" not in pokemon[0])]
     for pokemon in pokemons_without_mega:
         print(pokemon)
     return pokemons_without_mega
     
     # [wyrażenie for element in obiekt if warunek] [x**2 for x in range(10)]
 
-scrap_pokemon_list()
+pokemon_list = scrap_pokemon_list()
+for pokemon in pokemon_list:
+    try:
+        pokemon_info = get_pokemon_info(pokemon[1][3:])
+        print(pokemon)
+    except Exception as e:
+        print(pokemon[1], e)
