a122db6d164c27f90c0e9804c207c183cc7a3119
PiotrekP <kamiennygigant@gmail.com>
2025-08-14 19:44:21 +0200

    aktualizacja - lekcja 44

---
+++ Lekcja44/projekt.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from fpdf import FPDF
+
+def get_pokemon_info(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}/"
+    response = requests.get(url)
+    return response.json()
+
+def scrap_pokemon_list():
+    url = 'https://pokelife.pl/pokedex/index.php?title=Lista_Pokemon%C3%B3w_Kanto'
+    response = requests.get(url)
+    #print (response.content)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    pokemons_list = []
+    table = soup.find('table', class_ = 'Tabela1')
+    rows = table.find_all('tr')
+    for row in rows[1:]:
+        columns = row.find_all('td')
+        pokemon = (columns[2].text[:-1], f"No.{columns[0].text[:-1]}")
+        pokemons_list.append(pokemon)
+        #print(pokemon)
+    pokemons_without_mega = [pokemon for pokemon in pokemons_list if "Mega" not in pokemon[0]]
+    for pokemon in pokemons_without_mega:
+        print(pokemon)
+    return pokemons_without_mega
+    
+    # [wyrażenie for element in obiekt if warunek] [x**2 for x in range(10)]
+
+scrap_pokemon_list()
